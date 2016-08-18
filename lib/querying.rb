72654ad73b128ac2d8c25bd4ffedec47eb7ed004
parm530 <parmsahadeo530@gmail.com>
2016-08-17 21:42:15 -0400

    modified code to be more accurate

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,30 +1,31 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  " SELECT title, year FROM books
+  " SELECT books.title, books.year from books
     WHERE series_id = 1
     ORDER BY year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  " SELECT name, motto FROM characters
-    ORDER BY LENGTH(motto) DESC
+  " SELECT characters.name, characters.motto FROM characters
+    ORDER BY LENGTH(characters.motto) DESC
     LIMIT 1;"
 end
 
 def select_value_and_count_of_most_prolific_species
-  " SELECT species, COUNT(species) FROM characters
-    GROUP BY species 
-    ORDER BY COUNT(species) DESC
+  " SELECT characters.species, count(characters.species) FROM characters
+    GROUP BY characters.species
+    ORDER BY COUNT(characters.species) DESC
     LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
   " SELECT authors.name, subgenres.name FROM authors
-    JOIN subgenres ON subgenres.id = authors.id;"
+    JOIN series ON authors.id = series.author_id
+    JOIN subgenres ON series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
   " SELECT series.title FROM series
-    JOIN characters ON characters.series_id = series.id
+    JOIN characters ON series.id = characters.series_id
     WHERE characters.species = 'human'
     GROUP BY series.title
     ORDER BY COUNT(characters.species) DESC
@@ -32,9 +33,8 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  " SELECT characters.name, COUNT(books.title) FROM characters
-    JOIN character_books ON character_books.character_id = characters.id
-    JOIN books ON books.id = character_books.book_id
-    GROUP BY name
-    ORDER BY COUNT(books.title) DESC;"
+  " SELECT characters.name, COUNT(character_books.book_id) from characters
+    JOIN character_books ON characters.id = character_books.character_id
+    GROUP BY characters.name
+    ORDER BY COUNT(character_books.book_id) DESC;"
 end
